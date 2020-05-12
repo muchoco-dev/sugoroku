4c6927b7e36afbbcce9390aea8fc45b2fbad0410
stkzk3110 <fisherman0825dd@gmail.com>
2020-05-12 20:57:00 +0900

    #3 部屋一覧ページの作成

---
+++ src/resources/views/rooms.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Rooms</h1>
+            <button>部屋を作成する</button>
+        </div>
+    </div>
+</div>
+@endsection
