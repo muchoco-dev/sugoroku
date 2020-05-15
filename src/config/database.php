0bc9f25a6929a340004f2e6cc2cdcedbe4db5a26
stkzk3110 <fisherman0825dd@gmail.com>
2020-05-15 12:30:13 +0900

    #14 CircleCI用のDB設定追加

--- src/config/database.php
+++ src/config/database.php
@@ -91,6 +91,21 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'circleci' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circleci',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
+        ],
+
     ],
 
     /*
