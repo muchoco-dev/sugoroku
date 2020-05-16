934282be04438b2e35193ca0b0ee7df802621d1c
stkzk3110 <fisherman0825dd@gmail.com>
2020-05-17 00:32:06 +0900

    #31 room_userテーブルのクラス定義

--- src/app/Models/Room.php
+++ src/app/Models/Room.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    /**
+     * 役目を所有するユーザー
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User')
+            ->using('App\Models\RoomUser')
+            ->withPivot([
+                'go',
+                'status',
+                'position'
+            ]);
+    }
 }
