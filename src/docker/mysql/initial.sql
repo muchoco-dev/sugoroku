6f3fdf07573c871df6c531f9a7dfba689c45ac18
keitakn <keita.koga.work@gmail.com>
2020-06-09 23:40:19 +0900

    :recycle: #181 test用のDBユーザーのパスワードを通常のユーザーのパスワードと合わせて修正

--- src/docker/mysql/initial.sql
+++ src/docker/mysql/initial.sql
@@ -1,6 +1,6 @@
 -- 'sugoroku_test' というデータベースを作成
 -- 'sugoroku_test' というユーザー名のユーザーを作成
 -- データベース 'sugoroku_test' への権限を付与
-CREATE DATABASE IF NOT EXISTS sugoroku_test CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER sugoroku_test IDENTIFIED BY 'sugorokuPassword888';
+CREATE DATABASE IF NOT EXISTS sugoroku_test CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+CREATE USER sugoroku_test IDENTIFIED BY 'sugorokuPassword(888';
 GRANT ALL on sugoroku_test.* TO `sugoroku_test`@`%` WITH GRANT OPTION;
